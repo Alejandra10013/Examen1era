3a4f56934352e6e23dd64f02bc91a43f5cb6b980
Alejandra10013 <kalejac@gmail.com>
2017-12-30 17:08:50 -0300

    Examen

---
+++ Tienda/Controllers/ClientesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tienda.Models;
+
+namespace Tienda.Controllers
+{
+    public class ClientesController : Controller
+    {
+        VentaTiendaEntities cnx;
+
+        public ClientesController()
+        {
+            cnx = new VentaTiendaEntities();
+        }
+        public ActionResult NuevoCliente()
+        {
+
+            return View();
+        }
+        public ActionResult Listado()
+        {
+            cnx.Database.Connection.Open();
+            List<Clientes> cliente = cnx.Clientes.ToList();
+            cnx.Database.Connection.Close();
+            return View(cliente);
+        }
+        public ActionResult Guardar(int Rut, string Nombre, string Apellidos, string Direccion, string TipoCliente)
+        {
+            Clientes cliente = new Clientes()
+            {
+                Rut = Rut,
+                NombreCli = Nombre,
+                Apellido = Apellidos,
+                Direccion = Direccion,
+                TipoCliente = TipoCliente
+            };
+
+            cnx.Clientes.Add(cliente);
+            cnx.SaveChanges();
+            return View("Listado", cnx.Clientes.ToList());
+        }
+        public ActionResult Ficha(int Id)
+        {
+            return View(cnx.Clientes.Where(x => x.Id == Id).First());
+        }
+        
+        public ActionResult Ver(int Id)
+        {
+            return View("Ficha", null);
+        }
+    }
+}
---
+++ Tienda/Controllers/ProductosController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tienda.Models;
+
+namespace Tienda.Controllers
+{
+    public class ProductosController : Controller
+    {
+         VentaTiendaEntities cnx;
+
+         public ProductosController()
+         {
+             cnx = new VentaTiendaEntities();
+         }
+         public ActionResult NuevoProducto()
+         {
+             return View();
+         }
+         public ActionResult Listado()
+         {
+             cnx.Database.Connection.Open();
+             List<Productos> producto = cnx.Productos.ToList();
+             cnx.Database.Connection.Close();
+             return View(producto);
+         }
+        public ActionResult Guardar(string NombreProd, int CodigoBarra, string Familia, int Precio, int Descuento, string Descripcion)
+        {
+            Productos producto = new Productos()
+            {
+                NombreProd = NombreProd,
+                CodigoBarra = CodigoBarra,
+                Familia = Familia,
+                Precio = Precio,
+                Descuento = Descuento,
+                Descripcion = Descripcion
+            };
+
+            cnx.Productos.Add(producto);
+            cnx.SaveChanges();
+            return View("Listado", cnx.Productos.ToList());
+        }
+        public ActionResult Ficha(int Id)
+        {
+            return View(cnx.Productos.Where(x => x.Id == Id).First());
+        }
+        public ActionResult Ver(int Id)
+        {
+
+
+            return View("Ficha", null);
+        }
+    }
+}
